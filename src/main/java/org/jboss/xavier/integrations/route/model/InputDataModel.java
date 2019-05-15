be124208456ba9998614fc801b02ed3a7c9c2876
mrizzi <mrizzi@redhat.com>
2019-05-15 12:54:24 +0200

    Fixed InputDataModel to be Serializable

--- src/main/java/org/jboss/xavier/integrations/route/model/InputDataModel.java
+++ src/main/java/org/jboss/xavier/integrations/route/model/InputDataModel.java
@@ -3,9 +3,11 @@ package org.jboss.xavier.integrations.route.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class InputDataModel {
+public class InputDataModel implements Serializable {
     String customerId;
     String filename;
     Long numberOfHosts;
